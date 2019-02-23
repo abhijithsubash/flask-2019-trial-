bdc1a2711c43722fd199dc0043e7e9ca0b05a936
abhijithsubash <abhijithsubash788@gmail.com>
2019-02-23 14:33:54 +0530

    trial flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return "hello....."
+@app.route("/home")
+def home():
+        return "my home page"
+
+if(__name__=="__main__"):
+    app.run()
